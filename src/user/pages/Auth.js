a657a6738240885468ee10b83a3ed073f1cfa5ed
soniakash1998 <akashsoninit16@gmail.com>
2020-03-25 12:39:45 +0530

    Added the authentication page

---
+++ src/user/pages/Auth.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import Input from "../../shared/components/FormElements/Input";
+import { useForm } from "../../shared/hooks/form-hook";
+
+import "./Auth.css";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH
+} from "../../shared/util/validators";
+
+const Auth = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false
+      },
+      password: {
+        value: "",
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const authSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+  return (
+    <Card className="authentication" >
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="E-Mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid E-Mail."
+          onInput={inputHandler}
+        />
+
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid password, at least 5 characters."
+          onInput={inputHandler}
+        />
+
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
